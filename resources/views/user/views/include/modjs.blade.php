fe238fe060b3b4c0b24b284142864eaf41a98f76
azzamhfa <azzamuddienhanifa@gmail.com>
2021-08-09 12:47:10 +0700

    Azzam Change on ProfOrg

--- resources/views/user/views/include/modjs.blade.php
+++ resources/views/user/views/include/modjs.blade.php
@@ -1,24 +1,24 @@
 <!-- Modernizer JS -->
-<script src="user/assets/js/vendor/modernizr-3.6.0.min.js"></script>
+<script src={{ url("/user/assets/js/vendor/modernizr-3.6.0.min.js") }}></script>
 <!-- jQuery JS -->
-<script src="user/assets/js/vendor/jquery-3.3.1.min.js"></script>
+<script src={{ url("/user/assets/js/vendor/jquery-3.3.1.min.js") }}></script>
 <!-- Popper JS -->
-<script src="user/assets/js/vendor/popper.min.js"></script>
+<script src={{ url("/user/assets/js/vendor/popper.min.js") }}></script>
 <!-- Bootstrap JS -->
-<script src="user/assets/js/vendor/bootstrap.min.js"></script>
+<script src={{ url("/user/assets/js/vendor/bootstrap.min.js") }}></script>
 <!-- Slick Slider JS -->
-<script src="user/assets/js/plugins/slick.min.js"></script>
+<script src={{ url("/user/assets/js/plugins/slick.min.js") }}></script>
 <!-- nice select JS -->
-<script src="user/assets/js/plugins/nice-select.min.js"></script>
+<script src={{ url("/user/assets/js/plugins/nice-select.min.js") }}></script>
 <!-- audio video player JS -->
-<script src="user/assets/js/plugins/plyr.min.js"></script>
+<script src={{ url("/user/assets/js/plugins/plyr.min.js") }}></script>
 <!-- perfect scrollbar js -->
-<script src="user/assets/js/plugins/perfect-scrollbar.min.js"></script>
+<script src={{ url("/user/assets/js/plugins/perfect-scrollbar.min.js") }}></script>
 <!-- light gallery js -->
-<script src="user/assets/js/plugins/lightgallery-all.min.js"></script>
+<script src={{ url("/user/assets/js/plugins/lightgallery-all.min.js") }}></script>
 <!-- image loaded js -->
-<script src="user/assets/js/plugins/imagesloaded.pkgd.min.js"></script>
+<script src={{ url("/user/assets/js/plugins/imagesloaded.pkgd.min.js") }}></script>
 <!-- isotope filter js -->
-<script src="user/assets/js/plugins/isotope.pkgd.min.js"></script>
+<script src={{ url("/user/assets/js/plugins/isotope.pkgd.min.js") }}></script>
 <!-- Main JS -->
-<script src="user/assets/js/main.js"></script>
+<script src={{ url("/user/assets/js/main.js") }}></script>
